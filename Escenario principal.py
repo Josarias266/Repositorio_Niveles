99e4d3d0a0bacd7ae701c64010845c1a360f0218
José Arias <josarias266@gmail.com>
2021-09-24 15:51:15 -0600

    Base con enemigos JA

--- Escenario principal.py
+++ Escenario principal.py
@@ -1,34 +1,44 @@
-class villanos:
-
-
-    def __init__(self,nombre,hp,ap,escudo,sp,time):
-        self.nombre = nombre
+class obstaculo:  #aquí se define la clase principal para los obstáculos
+    def __init__(self, name, hp, ap, alive):  #aquí se define el constructor
+        self.name = name
         self.hp = hp
         self.ap = ap
-        self.escudo = escudo
-        self.sp = sp
-        self.time = time
+        self.alive = alive  #el estado por defecto del enemigo es "vivo"
 
-    def __str__(self):
-        return self.nombre
+    def matar(self):  #método para cambiar el estado del enemigo a "muerto"
+        self.alive = False
 
-    def atacar (self, jugador):
-        if jugador.escudo:
-            self.salud -= self.ataque
-
-
+    def estado(self):  #método para informar sobre el estado del enemigo
+        if (self.alive):
+            return "El enemigo " + (self.name) + " sigue con vida"
 
         else:
-            jugador.salud -= self.ataque
-        print("{} ataca a {}".format(self.nombre, jugador))
-        print("{} tiene de salud {}".format(self, self.salud))
-        print("{} tiene de salud {}".format(jugador, jugador.salud))
+            return "El enemigo " + (self.name) + " ha muerto"
 
 
-momia1 = villanos("momia", 20, 3, True) 
-zombie1 = villanos("zombie", 20, 5, False)
-jugador = villanos("jugador", 20, 3, True)
+pared = obstaculo("Mural", 10, 5, True)
+pared.matar()
+print(pared.estado())
+perro = obstaculo("Firulais", 7, 9, True)
+#perro.matar()
+print(perro.estado())
+dragon = obstaculo("Tohru", 20, 10, True)
+dragon.matar()
+print(dragon.estado())
+koopa = obstaculo("koopa-tropa", 20, 10, True)
+koopa.matar()
+print(koopa.estado())
 
+#Exportar datos a Json
+export = open("Prueba.json", "w")
+export.write("Estos son los resultados del nivel: \n")
+export.write(pared.estado() + "\n")
+export.write(perro.estado() + "\n")
+export.write(dragon.estado() + "\n")
+export.write(koopa.estado())
+export.close()
 
-momia1.atacar(jugador)
-jugador.atacar(momia1)
+#Pendiente evaluar si el nivel es superado al vencer obstaculos
+#Pendiente funcion para disminuir la resistencia
+#Pendiente funcion de nueva habilidad jugador
+#Pendiente nueva habilidad obstaculo
