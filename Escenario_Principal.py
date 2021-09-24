24c6f26acc1356bf5acee8943b267bb3096afad9
José Arias <josarias266@gmail.com>
2021-09-24 16:26:32 -0600

    Escenario Final 1

--- Escenario_Principal.py
+++ Escenario_Principal.py
@@ -15,7 +15,7 @@ class obstaculo:  #aquí se define la clase principal para los obstáculos
         else:
             return "El enemigo " + (self.name) + " ha muerto"
 
-
+#Objetos JA
 pared = obstaculo("Mural", 10, 5, True)
 pared.matar()
 print(pared.estado())
@@ -29,13 +29,43 @@ koopa = obstaculo("koopa-tropa", 20, 10, True)
 koopa.matar()
 print(koopa.estado())
 
+#Objetos FQ
+momia1 = obstaculo("momia", 50, 20, True)
+momia1.matar()
+print(momia1.estado())
+zombie1 = obstaculo("zombie", 40, 15, True)
+#zombie1.matar()
+print(zombie1.estado())
+
+#Objetos JP
+cuervo1 = obstaculo("Cuervo", 10000, 0, True)
+#cuervo1.matar()
+print(cuervo1.estado())
+araña1 = obstaculo("Spider" , 10000, 0, True)
+araña1.matar()
+print(araña1.estado())
+
+#Objetos RS
+coco1 = obstaculo("Farm", 35, 50, True)
+coco1.matar()
+print(coco1.estado())
+buitre1 = obstaculo("Jalks" , 25, 15, True)
+#buitre1.matar()
+print(buitre1.estado())
+
 #Exportar datos a Json
-export = open("Prueba.json", "w")
+export = open("Resultados_Nivel.json", "w")
 export.write("Estos son los resultados del nivel: \n")
 export.write(pared.estado() + "\n")
 export.write(perro.estado() + "\n")
 export.write(dragon.estado() + "\n")
-export.write(koopa.estado())
+export.write(koopa.estado() + "\n")
+export.write(momia1.estado() + "\n")
+export.write(zombie1.estado() + "\n")
+export.write(araña1.estado() + "\n")
+export.write(cuervo1.estado() + "\n")
+export.write(coco1.estado() + "\n")
+export.write(buitre1.estado())
 export.close()
 
 #Pendiente evaluar si el nivel es superado al vencer obstaculos
