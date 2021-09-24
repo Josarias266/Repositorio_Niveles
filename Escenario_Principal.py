b75000e31b8c2bd974b2b3aa84a7451ebf85c51d
José Arias <josarias266@gmail.com>
2021-09-24 15:26:08 -0600

    Base para Escenario Principal

--- Escenario_Principal.py
+++ Escenario_Principal.py
@@ -3,12 +3,12 @@ class obstaculo:  #aquí se define la clase principal para los obstáculos
         self.name = name
         self.hp = hp
         self.ap = ap
-        self.alive = alive  #el estado por defecto del enemigo es "vivo", es decir, "True"
+        self.alive = alive  #el estado por defecto del enemigo es "vivo", "True"
 
     def matar(self):  #método para cambiar el estado del enemigo a "muerto", "False"
         self.alive = False
 
-    def estado(self): #método para informar sobre el estado del enemigo
+    def estado(self):  #método para informar sobre el estado del enemigo
         if (self.alive):
             return "El enemigo " + (self.name) + " sigue con vida"
 
@@ -16,22 +16,27 @@ class obstaculo:  #aquí se define la clase principal para los obstáculos
             return "El enemigo " + (self.name) + " ha muerto"
 
 
-#Enemigos
 pared = obstaculo("Mural", 10, 5, True)
 pared.matar()
 print(pared.estado())
-
 perro = obstaculo("Firulais", 7, 9, True)
 #perro.matar()
 print(perro.estado())
-
 dragon = obstaculo("Tohru", 20, 10, True)
 dragon.matar()
 print(dragon.estado())
-
 koopa = obstaculo("koopa-tropa", 20, 10, True)
 koopa.matar()
 print(koopa.estado())
+
+#Exportar datos a Json
+export = open("Prueba.json", "w")
+export.write("Estos son los resultados del nivel: \n")
+export.write(pared.estado() + "\n")
+export.write(perro.estado() + "\n")
+export.write(dragon.estado() + "\n")
+export.write(koopa.estado())
+export.close()
 
 #Pendiente evaluar si el nivel es superado al vencer obstaculos
 #Pendiente funcion para disminuir la resistencia
