72e8221a07786d461250dd0d120cfedc962c8375
José Arias <josarias266@gmail.com>
2021-09-25 14:47:12 -0600

    Remasterización de selección de personaje a derrotar

--- Escenario_Principal.py
+++ Escenario_Principal.py
@@ -17,7 +17,7 @@ class obstaculo:  #aquí se define la clase principal para los obstáculos
 
 #Objetos JA
 pared = obstaculo("Whomp", 10, 5, True)
-pared.matar()
+#pared.matar()
 print(pared.estado())
 pokemon = obstaculo("Metapod", 7, 9, True)
 #pokemon.matar()
@@ -26,12 +26,12 @@ dragon = obstaculo("Tohru", 20, 10, True)
 #dragon.matar()
 print(dragon.estado())
 koopa = obstaculo("Koopa-Troopa", 15, 11, True)
-koopa.matar()
+#koopa.matar()
 print(koopa.estado())
 
 #Objetos FQ
 momia1 = obstaculo("momia", 50, 20, True)
-momia1.matar()
+#momia1.matar()
 print(momia1.estado())
 zombie1 = obstaculo("zombie", 40, 15, True)
 #zombie1.matar()
@@ -42,16 +42,24 @@ cuervo1 = obstaculo("Cuervo", 10000, 0, True)
 #cuervo1.matar()
 print(cuervo1.estado())
 araña1 = obstaculo("Spider" , 10000, 0, True)
-araña1.matar()
+#araña1.matar()
 print(araña1.estado())
 
 #Objetos RS
 coco1 = obstaculo("Farm", 35, 50, True)
-coco1.matar()
+#coco1.matar()
 print(coco1.estado())
 buitre1 = obstaculo("Jalks" , 25, 15, True)
 #buitre1.matar()
 print(buitre1.estado())
+
+#Objetos KA
+wolfman = obstaculo("Lucas", 7, 7, True)
+#wolfman.matar()
+print(wolfman.estado())
+vampire = obstaculo("Dracula", 8, 6, True)
+#vampire.matar()
+print(vampire.estado())
 
 #Exportar datos a Json
 export = open("Resultados_Nivel.json", "w")
@@ -65,7 +73,9 @@ export.write(zombie1.estado() + "\n")
 export.write(araña1.estado() + "\n")
 export.write(cuervo1.estado() + "\n")
 export.write(coco1.estado() + "\n")
-export.write(buitre1.estado())
+export.write(buitre1.estado() + "\n")
+export.write(wolfman.estado() + "\n")
+export.write(vampire.estado())
 export.close()
 
 #Pendiente evaluar si el nivel es superado al vencer obstaculos
