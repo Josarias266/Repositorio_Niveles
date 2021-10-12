a7daede1f0d3f0df97bcb4507771c785a45da846
José Arias <josarias266@gmail.com>
2021-10-12 15:46:29 -0600

    Selección de Avatar

--- Escenario_Principal.py
+++ Escenario_Principal.py
@@ -1,6 +1,6 @@
 import os
 
-class obstaculo:  #aquí se define la clase principal para los obstáculos
+class luchador:  #aquí se define la clase principal para los obstáculos
     def __init__(self, name, hp, ap, patada, manada, defbaja, defalta, selected, alive):  #aquí se define el constructor
         self.name = name
         self.hp = hp
@@ -9,8 +9,8 @@ class obstaculo:  #aquí se define la clase principal para los obstáculos
         self.manada = manada
         self.defbaja = defbaja
         self.defalta = defalta
-        self.selected = selected
-        self.alive = alive  #el estado por defecto del enemigo es "vivo", "True"
+        self.selected = selected    #Por defecto el luchador no está seleccionado
+        self.alive = alive  #el estado por defecto del luchador es "vivo", "True"
 
     def matar(self):  #método para cambiar el estado del enemigo a "muerto", "False"
         self.alive = False
@@ -23,21 +23,27 @@ class obstaculo:  #aquí se define la clase principal para los obstáculos
             return "El enemigo " + (self.name) + " sigue con vida"
         else:
             return "El enemigo " + (self.name) + " ha muerto"
+    
+    def escogido(self):
+        if (self.selected):
+            return "El luchador " + (self.name) + " ha sido seleccionado"
+        else:
+            return "El luchador " + (self.name) + " no ha sido seleccionado"
 
-chabelo = obstaculo("Chabelo", 10, 1, False, False, False, False, False, True)
-tecun = obstaculo("Tecún Uman", 10, 1, False, False, False, False, False, True)
-arjona = obstaculo("Ricardo Arjona", 10, 1, False, False, False, False, False, True)
-gaby = obstaculo("Gaby Moreno", 10, 1, False, False, False, False, False, True)
-mcgregor = obstaculo("Conor Mcgregor", 10, 1, False, False, False, False, False, True)
-jake = obstaculo("Jake Paul", 10, 1, False, False, False, False, False, True)
-reina = obstaculo("La Reina Izabel", 10, 1, False, False, False, False, False, True)
-siuu = obstaculo("El Bicho", 10, 1, False, False, False, False, False, True)
-jackie = obstaculo("Jackie Chan", 10, 1, False, False, False, False, False, True)
-buitre1 = obstaculo("Jalks", 10, 1, False, False, False, False, False, True)
-wolfman = obstaculo("Lucas", 10, 1, False, False, False, False, False, True)
+chabelo = luchador("Chabelo", 10, 1, False, False, False, False, False, True)
+tecun = luchador("Tecún Uman", 10, 1, False, False, False, False, False, True)
+arjona = luchador("Ricardo Arjona", 10, 1, False, False, False, False, False, True)
+gaby = luchador("Gaby Moreno", 10, 1, False, False, False, False, False, True)
+mcgregor = luchador("Conor Mcgregor", 10, 1, False, False, False, False, False, True)
+jake = luchador("Jake Paul", 10, 1, False, False, False, False, False, True)
+reina = luchador("La Reina Izabel", 10, 1, False, False, False, False, False, True)
+siuu = luchador("El Bicho", 10, 1, False, False, False, False, False, True)
+jackie = luchador("Jackie Chan", 10, 1, False, False, False, False, False, True)
+buitre1 = luchador("Jalks", 10, 1, False, False, False, False, False, True)
+wolfman = luchador("Lucas", 10, 1, False, False, False, False, False, True)
 
 print ("Bienvenido!")
-def selection():
+def selection():    #Sección 1, selección de personaje
     print ("Escoje tu luchador!")
     print ("Chabelo")
     print ("Tecún Uman")
@@ -54,12 +60,12 @@ def selection():
     def choise():
         selectionated.character()
 
-    choose = input("¿Qué enemigo quieres derrotar?: ")
+    choose = input("¿Qué avatar quieres utilizar?: ")
     choose = choose.upper()
     if choose == "CHABELO":
         selectionated = chabelo
         choise()
-    elif choose == "TECUN UMAN" or "TECÚN UMAN":
+    elif choose == "TECUN UMAN":
         selectionated = tecun
         choise()
     elif choose == "RICARDO ARJONA":
@@ -99,21 +105,21 @@ def selection():
     except NameError:
       input ("Presiona la tecla enter para ver los resultados...")
       os.system ("cls")
-      
+
 selection()
 
 #Estado final de los enemigos
-print(chabelo.estado())
-print(tecun.estado())
-print(arjona.estado())
-print(gaby.estado())
-print(mcgregor.estado())
-print(jake.estado())
-print(reina.estado())
-print(siuu.estado())
-print(jackie.estado())
-print(buitre1.estado())
-print(wolfman.estado())
+print(chabelo.escogido())
+print(tecun.escogido())
+print(arjona.escogido())
+print(gaby.escogido())
+print(mcgregor.escogido())
+print(jake.escogido())
+print(reina.escogido())
+print(siuu.escogido())
+print(jackie.escogido())
+print(buitre1.escogido())
+print(wolfman.escogido())
 
 input ("Presiona la tecla enter para salir...")
 
@@ -121,20 +127,20 @@ input ("Presiona la tecla enter para salir...")
 #Exportar datos a Json
 export = open("Resultados_Nivel.json", "w")
 export.write("Estos son los resultados del nivel: \n")
-export.write(chabelo.estado() + "\n")
-export.write(tecun.estado() + "\n")
-export.write(arjona.estado() + "\n")
-export.write(gaby.estado() + "\n")
-export.write(mcgregor.estado() + "\n")
-export.write(jake.estado() + "\n")
-export.write(siuu.estado() + "\n")
-export.write(reina.estado() + "\n")
-export.write(jackie.estado() + "\n")
-export.write(buitre1.estado() + "\n")
-export.write(wolfman.estado() + "\n")
+export.write(chabelo.escogido() + "\n")
+export.write(tecun.escogido() + "\n")
+export.write(arjona.escogido() + "\n")
+export.write(gaby.escogido() + "\n")
+export.write(mcgregor.escogido() + "\n")
+export.write(jake.escogido() + "\n")
+export.write(siuu.escogido() + "\n")
+export.write(reina.escogido() + "\n")
+export.write(jackie.escogido() + "\n")
+export.write(buitre1.escogido() + "\n")
+export.write(wolfman.escogido() + "\n")
 export.close()
 
-#Pendiente evaluar si el nivel es superado al vencer obstaculos
+#Pendiente evaluar si el nivel es superado al vencer luchadores
 #Pendiente funcion para disminuir la resistencia
 #Pendiente funcion de nueva habilidad jugador
-#Pendiente nueva habilidad obstaculo
+#Pendiente nueva habilidad luchador
